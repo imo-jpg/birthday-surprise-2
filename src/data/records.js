56f0ac8b50db8359a9119aaf2840c121c00e07fa
imo-jpg <imo.todd@gmail.com>
2023-04-10 18:06:02 -0400

    adds new record covers and messages

--- src/data/records.js
+++ src/data/records.js
@@ -3,6 +3,9 @@ import Gangs from './Gangs.svg';
 import Houses from './Houses of the Holy.svg';
 import Here from './Here Comes the Flood.svg';
 import Ga from './Ga Ga Ga Ga Ga.svg';
+import Save from './save_it_for_later.svg';
+import Lied from './Lied.svg';
+import Uncontrollable from './uncontrollable_urge.svg';
 
 export const records = [
     {
@@ -50,4 +53,31 @@ export const records = [
         source: Houses,
         alt: "a pixel art rendition of the album cover for Houses of the Holy by Led Zeppelin"
     },
+    {
+        id: 5,
+        title: "Save it for Later",
+        artist: "English Beat",
+        message: "Happy birthday, Peter!",
+        sender: "Rob",
+        source: Save,
+        alt: "a pixel art rendition of the album cover for Save it for Later by the English Beat"
+    },
+    {
+        id: 6,
+        title: "4 Lieder, Op. 27: 4. Morgen!",
+        artist: "Richard Strauss with Jessye Norman",
+        message: "Happy birthday, Peter!",
+        sender: "the von Maydells",
+        source: Lied,
+        alt: "a pixel art rendition of the album cover for Strauss, R.: 4 Last Songs by Richard Strauss"
+    },
+    {
+        id: 7,
+        title: "Uncontrollable Urge",
+        artist: "DEVO",
+        message: "Happy birthday, Peter!",
+        sender: "Jaclyn",
+        source: Uncontrollable,
+        alt: "a pixel art rendition of the album cover for Q: Are We Not Men? A: We Are Devo! by DEVO"
+    }
 ]
